5f8f4b06d207de8dcbf74923b343b9377036bcd3
James Smith <james.smith@djalbat.com>
2020-02-26 17:05:32 +0000

    Corrected the bin entry in the package.json file.

--- bin/callback/options.js
+++ bin/callback/options.js
@@ -1,9 +1,44 @@
 'use strict';
 
+const messages = require('../messages');
+
+const { NO_ENTRY_FILE_MESSAGE,
+        NO_BUNDLE_FILE_MESSAGE,
+        NO_OUTPUT_DIRECTORY_MESSAGE } = messages;
+
 function optionsCallback(proceed, abort, context) {
   const { options } = context,
-        { debug, entryFile, bundleFile, inputDirectory, outputDirectory } = options,
-        sourceMaps = debug ?
+        { debug, entryFile, bundleFile, inputDirectory, outputDirectory } = options;
+
+  if (!outputDirectory) {
+    console.log(NO_OUTPUT_DIRECTORY_MESSAGE);
+
+    abort();
+
+    return;
+  }
+
+  if (bundleFile) {
+    if (!entryFile) {
+      console.log(NO_ENTRY_FILE_MESSAGE);
+
+      abort();
+
+      return;
+    }
+  }
+
+  if (entryFile) {
+    if (!bundleFile) {
+      console.log(NO_BUNDLE_FILE_MESSAGE);
+
+      abort();
+
+      return;
+    }
+  }
+
+  const sourceMaps = debug ?
                       'inline' :
                          null,
         babelOptions = {
--- bin/messages.js
+++ bin/messages.js
@@ -2,18 +2,24 @@
 
 const BATCH_FAILED_MESSAGE = 'The build failed.',
       BABEL_FAILED_MESSAGE = 'Babel failed:',
+      NO_ENTRY_FILE_MESSAGE = 'No entry file has been specified.',
+      NO_BUNDLE_FILE_MESSAGE = 'No bundle file has been specified.',
       ENTRY_FILE_NOT_INCLUDED = 'The entry file is not included in the transformed files.',
       BATCH_SUCCESSFUL_MESSAGE = 'Successful build.',
       BROWSERIFY_NOT_INSTALLED = 'Browserify is not installed.',
       BABEL_CORE_NOT_INSTALLED = 'The \'@babel/core\' package is not installed.',
-      BROWSERIFY_FAILED_MESSAGE = 'Browserify failed:';
+      BROWSERIFY_FAILED_MESSAGE = 'Browserify failed:',
+      NO_OUTPUT_DIRECTORY_MESSAGE = 'No output directory has been specified.';
 
 module.exports = {
   BATCH_FAILED_MESSAGE,
   BABEL_FAILED_MESSAGE,
+  NO_ENTRY_FILE_MESSAGE,
+  NO_BUNDLE_FILE_MESSAGE,
   ENTRY_FILE_NOT_INCLUDED,
   BATCH_SUCCESSFUL_MESSAGE,
   BROWSERIFY_NOT_INSTALLED,
   BABEL_CORE_NOT_INSTALLED,
-  BROWSERIFY_FAILED_MESSAGE
+  BROWSERIFY_FAILED_MESSAGE,
+  NO_OUTPUT_DIRECTORY_MESSAGE
 };
