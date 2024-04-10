9bde581c9c82cd99dc0146f84363fa26721a3321
James Smith <james.smith@djalbat.com>
2024-04-10 19:43:39 +0100

    Done away with the main file.

--- bin/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const actions = require("./actions");
-
-function main(command, argument, options) {
-  actions(command, argument, options);
-}
-
-module.exports = main;
--- watchful.js
+++ watchful.js
@@ -3,7 +3,7 @@
 const { parseArgv } = require("argumentative"),
       { arrayUtilities } = require("necessary");
 
-const main = require("./bin/main"),
+const actions = require("./bin/actions"),
       abbreviations = require("./bin/abbreviations");
 
 const { first, second } = arrayUtilities;
@@ -14,4 +14,4 @@ const { commands, options } = parseArgv(process.argv, abbreviations),
       command = firstCommand || null, ///
       argument = secondCommand || null; ///
 
-main(command, argument, options);
+actions(command, argument, options);
